bb6eed5c2a34408aa1aa77696bb88f6cb0c3d4f4
Sanansadig <sanan.sadig@hotmail.com>
2017-05-05 14:21:54 +0400

    VizaFinal

---
+++ VizaFinal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizaFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vizani Daxil Et");
+            int viza = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Finali Daxil Et");
+            int final = Convert.ToInt32(Console.ReadLine());
+            double netice = Convert.ToDouble(final)*0.6 + Convert.ToDouble(viza)*0.4; 
+
+            if (netice>=45)
+            {
+                Console.WriteLine(netice + "Kecdiniz");
+
+            }
+            else
+            {
+                Console.WriteLine(netice + "Kesildiz");
+            }
+        }
+    }
+}
